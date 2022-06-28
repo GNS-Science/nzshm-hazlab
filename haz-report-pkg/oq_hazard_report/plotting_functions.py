d419487c48b766ed470b2e8325187c6e9e840df7
chrisdicaprio <christopher.dicaprio@gmail.com>
2022-06-28 21:18:08 +1200

    experimenting with plot styles

--- haz-report-pkg/oq_hazard_report/plotting_functions.py
+++ haz-report-pkg/oq_hazard_report/plotting_functions.py
@@ -16,9 +16,13 @@ def plot_hazard_curve_wunc(hazard_data, location, imt, ax, xlim, ylim):
         # alpha = min(1.0,(len(aggs)/2.0 - np.abs(len(aggs)/2.0 - i)) / (len(aggs)/2.0)+0.25)
         # alpha = min(1.0,-(2.0/len(aggs))**2 * (i-len(aggs)/2.0)**2  + 1.2)
         # alpha = max(0.0,(len(aggs)/2.0 - np.abs(len(aggs)/2.0 - i)) / (len(aggs)/2.0)-0.1)
-        alpha = max(0.0,(len(aggs)/2.0 - np.abs(len(aggs)/2.0 - i)) / (len(aggs)/2.0))
+        # alpha = max(0.0,(len(aggs)/2.0 - np.abs(len(aggs)/2.0 - i)) / (len(aggs)/2.0))
+        # alpha = (len(aggs)/2.0 + np.abs(len(aggs)/2.0 - i)) / (len(aggs)/2.0) - 1.0
+        alpha = max(0.5,(len(aggs)/2.0 + np.abs(len(aggs)/2.0 - i)) / (len(aggs)/2.0) - 1.0)
+        print(alpha)
         vals = calculate_agg(hazard_data,location,imt,agg)
-        ax.plot(lvls,vals,color=str(alpha),alpha=0.6,lw=1)
+        # ax.plot(lvls,vals,color=str(alpha),alpha=0.6,lw=1)
+        ax.plot(lvls,vals,color=str(alpha),lw=1)
                 
     vals = calculate_agg(hazard_data,location,imt,0.5)
     ax.plot(lvls,vals,'b',alpha=.8,lw=2)
