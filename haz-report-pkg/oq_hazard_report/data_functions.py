8ef29d0e5d5ff3ebaa1235cc7322b84f95da3801
chrisdicaprio <christopher.dicaprio@gmail.com>
2022-06-29 17:33:21 +1200

    remove get_lv function

--- haz-report-pkg/oq_hazard_report/data_functions.py
+++ haz-report-pkg/oq_hazard_report/data_functions.py
@@ -1,21 +1,5 @@
 import numpy as np
-from toshi_hazard_store import model, query
 
-def get_lv(toshi_id,imt,site_list,aggrigations):
-    
-    xy = {}
-    vs30 = 250 #TODO Vs30 arg
-    
-    res = query.get_hazard_curves_stats(toshi_id, vs30, imt, site_list, aggrigations)
-
-    for site in site_list:
-        xy[site] = {}
-        
-    for r in res:
-        xy[r.location_code][r.aggregation] = np.array([[p.level,p.value] for p in r.lvl_val_pairs])
-
-    return xy
-    
 def weighted_quantile(values, quantiles, sample_weight=None, 
                       values_sorted=False, old_style=False):
     """ Very close to numpy.percentile, but supports weights.
