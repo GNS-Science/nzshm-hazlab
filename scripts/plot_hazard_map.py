a8b717db6202457a9742261f1bb2b69f98193bb9
chrisdicaprio <christopher.dicaprio@gmail.com>
2022-12-13 14:13:53 +1300

    big maps

--- scripts/plot_hazard_map.py
+++ scripts/plot_hazard_map.py
@@ -73,7 +73,6 @@ def get_poe_grid(thp_id, site_list, imt, agg, poe, vs30):
 
     haz_poe = haz_poe[['lat','lon','haz-poe']]
     haz_poe.to_csv('flat_hazardmap_400_02_mean.csv')
-    breakpoint()
 
     haz_poe = haz_poe.pivot(index="lat", columns="lon")
     haz_poe = haz_poe.droplevel(0, axis=1)
@@ -114,6 +113,9 @@ agg = 'mean'
 # poes = [0.02, 0.1]
 poes = [0.02]
 vs30s = [400]
+filetype = 'pdf'
+plot_width = 78 
+font_size = 48 
 
 plot_faults = True
 colormap = 'inferno' # 'viridis', 'jet', 'plasma', 'imola', 'hawaii'
@@ -163,26 +165,30 @@ acc_min = {
     },
 }
 
-region="163/185/-50/-24" if plot_faults else "165/180/-48/-34"
+# rregion="163/185/-50/-24" if plot_faults else "165/180/-48/-34"
+region = "165/180/-48/-34"
+font = f'{font_size}p'
 
-
-
+projection = f'M{plot_width}c'
 #============================================================================================================
-
+pygmt.config(FONT_LABEL='48p', FONT_ANNOT_PRIMARY='36p')
 for vs30 in vs30s:
     for imt in imts:
         for poe in poes:
             full_dir = Path(fig_dir,f'{int(vs30)}')
             grid = get_poe_grid(thp_id, site_list, imt, agg, poe, vs30)
+            # dgrid = pygmt.grdgradient(grid=grid, radiance=[270, 30])
             fig = pygmt.Figure()
-            pygmt.config(FONT_ANNOT_PRIMARY = 14)
             # pygmt.makecpt(cmap = colormap, log=True, series=[np.log10(acc_min[vs30][imt]),np.log10(acc_max[vs30][imt]),.1])
             pygmt.makecpt(cmap = colormap, log=True, series=[np.log10(0.1),np.log10(3),.1])
-
-            print(f'vs30-{vs30}, poe-{poe}, imt-{imt}: max shaking: {grid.max()}')
-            fig.grdimage(grid=grid, region=region, projection="M15c", cmap = True, dpi = 100, frame = "a")
+                    
+            # print(f'vs30-{vs30}, poe-{poe}, imt-{imt}: max shaking: {grid.max()}')
+            fig.grdimage(grid=grid, region=region, projection=projection, cmap = True, dpi = 100, frame = "a")
+            # fig.grdimage(grid=grid, region=region, projection=projection, cmap = True, frame = "a")
+            # fig.coast(shorelines = True, water="white", region=region, projection=projection, frame = "a")
             fig.coast(shorelines = True, water="white")
-            fig.basemap(frame=["a", f"+t{vs30}m/s {imt} {poe*100:.0f}% in 50 yrs"])
+            # fig.basemap(frame=["a", f"+t{vs30}m/s {imt} {poe*100:.0f}% in 50 yrs"])
+            fig.basemap(frame=["a"])
             if plot_faults:
                 faults, backarc, hikurangi, puysegur = load_polygons()
                 fig.plot(data=hikurangi,color="220/220/220",transparency=60,pen="black")
@@ -190,12 +196,14 @@ for vs30 in vs30s:
                 fig.plot(data=faults)
                 # fig.plot(data=backarc, pen="1p,red")
                 # filepath = Path(full_dir,f'{thp_id}-{vs30}-{imt}-{poe}_faults.png')
-                filepath = Path(full_dir,'just_faults.png')
+                filepath = Path(full_dir,f'{thp_id}-{vs30}-{imt}-{poe}_faults.{filetype}')
+                # filepath = Path(full_dir,'just_faults.png')
             else:
-                filepath = Path(full_dir,f'{thp_id}-{vs30}-{imt}-{poe}.png')
-
-            fig.colorbar(frame=f'af+l"{imt} ({poe*100:.0f}% PoE in 50)"')#,position='+ef')
-
-            # fig.savefig(filepath)
-            fig.show()
-
+                filepath = Path(full_dir,f'{thp_id}-{vs30}-{imt}-{poe}.{filetype}')
+                    
+            fig.colorbar(xshift='40c', yshift='10c', position='+w27c/3c+h', frame=f'af+l"{imt} ({poe*100:.0f}% PoE in 50)"')#,position='+ef')
+                    
+            fig.savefig(filepath)
+            # fig.show()
+                    
+                    
