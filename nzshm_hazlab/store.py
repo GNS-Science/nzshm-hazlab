69fc2c8f6c763116ef2b00d97b245b2110605d98
chrisdicaprio <christopher.dicaprio@gmail.com>
2023-02-03 14:08:09 +1300

    improvements to store, WIP

--- nzshm_hazlab/store.py
+++ nzshm_hazlab/store.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from zipfile import ZIP_BZIP2, ZipFile
 from pandas import DataFrame
 from pathlib import Path
 from typing import List, Any
@@ -14,6 +15,8 @@ ARCHIVE_DIR = Path(os.environ['HAZARD_CURVE_ARCHIVE'])
 assert ARCHIVE_DIR.exists()
 DTYPE = {'lat':str,'lon':str}
 SITE_LIST = 'NZ_0_1_NB_1_1'
+IMTS = ['PGA', 'SA(0.2)', 'SA(0.5)', 'SA(1.0)', 'SA(1.5)', 'SA(2.0)', 'SA(3.0)', 'SA(5.0)', 'SA(10.0)']
+AGGS = ["mean", "0.005", "0.01", "0.025", "0.05", "0.1", "0.2", "0.5", "0.8", "0.9", "0.95", "0.975", "0.99", "0.995"]
 
 def all_locations() -> List[str]:
     locations_nz35 = [
@@ -27,13 +30,11 @@ def all_locations() -> List[str]:
         CodedLocation( *loc, 0.001).code
         for loc in grid_locs
     ]
-    locations_grid = []
-
     return locations_nz35 + locations_grid
 
-def archive_filepath(hazard_id: str, vs30: int) -> Path:
-    return Path(ARCHIVE_DIR, f'{hazard_id}-{vs30}.json')
 
+def archive_filepath(hazard_id: str, vs30: int) -> Path:
+    return Path(ARCHIVE_DIR, f'{hazard_id}-{vs30}.bz2')
 
 def download_hazard(hazard_id: str, vs30: int) -> None:
     """download all locations, imts and aggs for a particular hazard_id and vs30. Save in DataFrame written to disk"""
@@ -42,6 +43,8 @@ def download_hazard(hazard_id: str, vs30: int) -> None:
         hazard_model_ids=[hazard_id],
         vs30s = [vs30],
         locs = all_locations()[:1],
+        imts = IMTS,
+        aggs = AGGS,
     )
     imts = set() 
     aggs = set()
@@ -57,8 +60,8 @@ def download_hazard(hazard_id: str, vs30: int) -> None:
     index = range(len(all_locations()) * nimts * naggs * nlevels)
     hazard_curves = pd.DataFrame(columns=columns, index=index)
     ind = 0
-    for i,res in enumerate(get_hazard_curves(all_locations(), [vs30], [hazard_id] )):
-        print(f'retrieving record {i} from THS') if i%100 == 0 else None
+    for i,res in enumerate(get_hazard_curves(all_locations(), [vs30], [hazard_id], IMTS, AGGS)):
+        print(f'retrieving record {i} from THS') if i%10000 == 0 else None
         lat = f'{res.lat:0.3f}'
         lon = f'{res.lon:0.3f}'
         for value in res.values:
@@ -111,8 +114,19 @@ def get_hazard(
 
 if __name__ == "__main__":
 
-    hazard_id = "NSHM_v1.0.1_CRUsens_baseline"
+    hazard_ids = [
+        "NSHM_v1.0.1_CRUsens_baseline",
+        "NSHM_v1.0.1_sens_jump5km",
+        "NSHM_v1.0.1_sens_jump10km",
+        "NSHM_v1.0.1_sens_jump10km_iso",
+        "NSHM_v1.0.1_sens_jump5km_iso",
+        "NSHM_v1.0.1_CRUsens_baseline_iso",
+    ]
+    hazard_ids = hazard_ids[1:]
 
     keep = ['WLG','AKL','CHC']
     locations = [CodedLocation(loc['latitude'], loc['longitude'], 0.001) for loc in LOCATIONS_BY_ID.values() if loc['id'] in keep]
-    hazard_curves = get_hazard(hazard_id, locations, 400, ['PGA', 'SA(0.5)' ], ['mean'])
+    for id in hazard_ids:
+        print(f'working on {id}')
+        # hazard_curves = get_hazard(id, locations, 400, ['PGA', 'SA(0.5)' ], ['mean'])
+        download_hazard(id, 400)
