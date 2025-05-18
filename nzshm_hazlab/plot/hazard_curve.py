987d0ffe0bc2dd7fb7051b999efbf9ff2c9bdca5
chrisdicaprio <christopher.dicaprio@gmail.com>
2025-05-19 11:03:27 +1200

    treat label serperatly from other kwargs; return list of line handles

--- nzshm_hazlab/plot/hazard_curve.py
+++ nzshm_hazlab/plot/hazard_curve.py
@@ -4,6 +4,7 @@ import nzshm_hazlab.plot.constants as constants
 
 if TYPE_CHECKING:
     from matplotlib.axes import Axes
+    from matplotlib.lines import Line2D
     from nzshm_common import CodedLocation
 
     from nzshm_hazlab.data import HazardCurves
@@ -18,10 +19,13 @@ def plot_hazard_curve(
     vs30: int,
     aggs: list[str],
     **kwargs,  # color, linestyle, label, etc
-):
+) -> list['Line2D']:
 
     color = kwargs.pop("color", None)
     color = color if color else constants.DEFAULT_COLOR
+
+    label = kwargs.pop("label", None)
+    line_handles = []
 
     # if odd number of aggs, plot the centre as a thick line
     i_center = int(len(aggs) / 2)
@@ -29,7 +33,8 @@ def plot_hazard_curve(
         levels, probs = data.get_hcurve(
             hazard_model_id, imt, location, aggs[i_center], vs30
         )
-        axes.plot(levels, probs, lw=constants.LINE_WIDTH_CENTER, color=color, **kwargs)
+        lhs = axes.plot(levels, probs, lw=constants.LINE_WIDTH_CENTER, color=color, label=label, **kwargs)
+        line_handles += lhs
 
     filled = False
     for i in range(1, i_center + 1):
@@ -39,16 +44,19 @@ def plot_hazard_curve(
         levels_high, probs_high = data.get_hcurve(
             hazard_model_id, imt, location, aggs[i_center + i], vs30
         )
-        axes.plot(
+        lhs = axes.plot(
             levels_low, probs_low, lw=constants.LINE_WIDTH_BOUNDS, color=color, **kwargs
         )
-        axes.plot(
+        line_handles += lhs
+
+        lhs = axes.plot(
             levels_high,
             probs_high,
             lw=constants.LINE_WIDTH_BOUNDS,
             color=color,
             **kwargs,
         )
+        line_handles += lhs
 
         if not filled:
             axes.fill_between(
@@ -68,3 +76,5 @@ def plot_hazard_curve(
     )
     axes.tick_params(axis="both", which="major", labelsize=constants.TICK_FONTSIZE)
     axes.grid(color=constants.GRID_COLOR)
+
+    return line_handles
