29fa502833ea591136d28599c8c1d7b5c6c0b923
chrisdicaprio <christopher.dicaprio@gmail.com>
2025-05-21 10:43:02 +1200

    versioning

--- nzshm_hazlab/__init__.py
+++ nzshm_hazlab/__init__.py
@@ -3,3 +3,5 @@
 This library provides tools for accessing and plotting aggregated hazard curves (e.g. produced by
 toshi-hazard-post or OpenQuake), calculated deritivate products such as UHS.
 """
+
+__version__ = '0.1.0'
