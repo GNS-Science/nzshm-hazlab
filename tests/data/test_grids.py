94e11eb3910e2ddfbae43b3fc9ef9d7c2d509a72
chrisdicaprio <christopher.dicaprio@gmail.com>
2025-07-10 10:49:18 +1200

    move reused object to pytest fixture

--- tests/data/test_grids.py
+++ tests/data/test_grids.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import pytest
 
 import numpy as np
 from toshi_hazard_store.model import ProbabilityEnum
@@ -9,14 +10,24 @@ from nzshm_hazlab.data import HazardGrids
 class DummyGridLoader:
     """A dummy grid loader class."""
 
+    def __init__(self, directory: Path | str):
+        self._directory = Path(directory)
+
     def get_grid(self, hazard_model_id, imt, grid_name, vs30, poe, agg: str):
         """A method for the dummy loader."""
         filename = f"hazard_grid-{hazard_model_id}-{imt}-{grid_name}-{vs30}-{poe.name}-{agg}.npy"
-        filepath = Path(__file__).parent.parent / f"fixtures/data/grids/{filename}"
+        filepath = self._directory / filename
         return np.load(filepath)
 
+# scope is function because we attach a mocker.spy to the object which only seems to work for function scope
+@pytest.fixture(scope='function')
+def hazard_grids() -> HazardGrids:
+    loader = DummyGridLoader(Path(__file__).parent.parent / "fixtures/data/grids/")
+    return HazardGrids(loader=loader)
 
-def test_get_grid():
+
+
+def test_get_grid(hazard_grids):
     hazard_model_id = "NSHM_v1.0.4"
     grid_name = "NZ_0_1_NB_1_1"
     poe = ProbabilityEnum._2_PCT_IN_50YRS
@@ -24,14 +35,12 @@ def test_get_grid():
     imt = "SA(1.0)"
     vs30 = 750
 
-    loader = DummyGridLoader()
-    hazard_grids = HazardGrids(loader=loader)
 
     imtls = hazard_grids.get_grid(hazard_model_id, imt, grid_name, vs30, poe, agg)
     assert imtls.shape == (3741,)
 
 
-def test_get_grid_cache(mocker):
+def test_get_grid_cache(mocker, hazard_grids):
     """Subsequent calls to get_grid should not call _load_data if the data have already been loaded."""
     hazard_model_id = "NSHM_v1.0.4"
     grid_name = "NZ_0_1_NB_1_1"
@@ -40,8 +49,6 @@ def test_get_grid_cache(mocker):
     imt = "SA(1.0)"
     vs30 = 750
 
-    loader = DummyGridLoader()
-    hazard_grids = HazardGrids(loader=loader)
     spy = mocker.spy(hazard_grids, '_load_data')
 
     hazard_grids.get_grid(hazard_model_id, imt, grid_name, vs30, poe, agg)
@@ -56,7 +63,7 @@ def test_get_grid_cache(mocker):
     assert spy.call_count == 2
 
 
-def test_2_grids():
+def test_2_grids(hazard_grids):
     """Test that we can get two different grids and the second doesn't overwrite the first."""
     hazard_model_id = "NSHM_v1.0.4"
     grid_name = "NZ_0_1_NB_1_1"
@@ -67,9 +74,6 @@ def test_2_grids():
     poe2 = ProbabilityEnum._10_PCT_IN_50YRS
     imt2 = "PGA"
     vs302 = 400
-
-    loader = DummyGridLoader()
-    hazard_grids = HazardGrids(loader=loader)
 
     grid1 = hazard_grids.get_grid(hazard_model_id, imt1, grid_name, vs301, poe1, agg)
     grid2 = hazard_grids.get_grid(hazard_model_id, imt2, grid_name, vs302, poe2, agg)
